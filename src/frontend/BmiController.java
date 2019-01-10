4eafa43827a369bf314e9337e96e9c26d3a6eabb
SimonIT <simonit.orig@gmail.com>
2019-01-10 08:54:20 +0100

    add sex

--- src/frontend/BmiController.java
+++ src/frontend/BmiController.java
@@ -1,13 +1,24 @@
 package frontend;
 
 import backend.Sex;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 
-public class BmiController {
+import java.net.URL;
+import java.util.Arrays;
+import java.util.ResourceBundle;
+
+public class BmiController implements Initializable {
 
     @FXML
     ComboBox<Sex> sexComboBox;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        sexComboBox.setItems(FXCollections.observableList(Arrays.asList(Sex.values())));
+    }
 
     void calculate(){
 
